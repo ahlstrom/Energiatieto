22aace86dd4a085d52bf312cccd5dcd8a73ce4d9
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-10-26 09:38:07 +0300

    remove unnecessary config settings

--- src/config.js
+++ src/config.js
@@ -9,9 +9,6 @@ module.exports = function(app, express) {
         app.set('view engine', 'jade');
         app.set('view options', { layout: false });
 
-        app.use(express.bodyParser());
-        app.use(express.cookieParser());
-        app.use(express.methodOverride());
         app.use(app.router);
         app.use(express['static'](__dirname + '/../public'));
     });
