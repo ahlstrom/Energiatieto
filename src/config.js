2aae31839c88c722f6571272f7f5677303838a91
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-10-26 09:56:02 +0300

    use different static dir for production

--- src/config.js
+++ src/config.js
@@ -10,6 +10,15 @@ module.exports = function(app, express) {
         app.set('view options', { layout: false });
 
         app.use(app.router);
-        app.use(express['static'](__dirname + '/../public'));
+
+        var staticdir = __dirname + '/../public';
+
+        console.log('environment: ' + app.settings.env);
+        if ('development' === app.settings.env) {
+            app.use(express['static'](staticdir));
+        } else {
+            app.use(express['static'](staticdir + '/dist'));
+        }
+
     });
 };
