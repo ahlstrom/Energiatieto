4ec696c672c3ab9da5571577ccbe6e989a90b799
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-10-29 12:53:53 +0200

    use specifically created index view to deliver packaged js in production environment

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@
 
 module.exports = function(app){
 
+  app.get('/', function(req, res) {
+    res.render('index.' + app.settings.env + '.jade');
+  });
+
   require('./api/routes')(app, '/api');
 
 }; 
