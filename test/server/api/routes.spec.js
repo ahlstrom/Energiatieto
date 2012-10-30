9d179c7b6fd549ad48279ac0d0f18dd7fceb5e6b
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-10-30 12:35:37 +0200

    remove unnecessary logging

--- test/server/api/routes.spec.js
+++ test/server/api/routes.spec.js
@@ -5,7 +5,6 @@ var assert = require('assert');
 describe('test', function() {
     describe('#test()', function() {
         it('should be fine', function() {
-            console.log(__dirname);
             assert.equal(true, true);
         });
     });
