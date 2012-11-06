daf1da7bc640b9d500280415953b3b592013ef7f
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-11-06 16:16:08 +0200

    use fork instead of exec

--- tasks/server.js
+++ tasks/server.js
@@ -1,18 +1,23 @@
 "use strict";
 
-var exec = require('child_process').exec;
+var fork = require('child_process').fork;
 
 module.exports = function(grunt) {
     var child;
     grunt.registerTask("functional", "Run functional tests.", function(prop) {
         var args = this.args;
-
-        child = exec('node src/web', {
-            env: {
-                "PORT": "5050"
-            }
+        console.log(__dirname + '/..');
+        child = fork('src/web', 
+            {
+                env: {
+                    "PORT": "5050"
+                }
+            });
+        
+        process.on('exit', function() {
+            child.kill('SIGTERM');
         });
-
+        
         grunt.task.run('simplemocha');
         grunt.task.run('shutdown-functional-server');
     });
