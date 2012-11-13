9256c8a55e3500f071a7b8bb0b87fbc5c10a7119
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-11-13 15:02:30 +0200

    remove unnecessary geocode roundtrip

--- src/app/js/views/map/buildinglayer.js
+++ src/app/js/views/map/buildinglayer.js
@@ -38,51 +38,44 @@ define([
             });
 
             google.maps.event.addListener(layer, 'click', function(event) {
-                new google.maps.Geocoder().geocode({
-                        latLng: event.latLng
-                    },
-                    function(res, status) {
-                        var byggid = event.row.ByggID.value;
-                        var findByByggId = function(it) { return it.get("byggid") == byggid; };
+                var byggid = event.row.ByggID.value;
+                var findByByggId = function(it) { return it.get("byggid") == byggid; };
 
-                        var building = collection.find(findByByggId);
+                var building = collection.find(findByByggId);
 
-                        if (building === null || typeof building === 'undefined') {
-                            building = new Building({
-                                byggid: byggid,
-                                averageRadiation: event.row.AvActKWHm2.value,
-                                roofArea: event.row.area.value,
-                                address: res[0],
-                                location: event.latLng
-                            });
-
-                            var marker = new google.maps.Marker({
-                                icon: inactiveIcon,
-                                position: event.latLng,
-                                map: map
-                            });
-
-                            building.on("deselect", function() {
-                                marker.setIcon(inactiveIcon);
-                            });
-
-                            var selectBuilding = function() {
-                                marker.setIcon(activeIcon);
-                                collection.trigger("select", building);
-                            };
-                            
-                            google.maps.event.addListener(marker, 'click', selectBuilding);
-
-                            building.on("destroy", function() {
-                                marker.setMap(null);
-                            });
-
-                            collection.add(building);
-
-                            selectBuilding();
-                        }
-
+                if (building === null || typeof building === 'undefined') {
+                    building = new Building({
+                        byggid: byggid,
+                        averageRadiation: event.row.AvActKWHm2.value,
+                        roofArea: event.row.area.value,
+                        location: event.latLng
                     });
+
+                    var marker = new google.maps.Marker({
+                        icon: inactiveIcon,
+                        position: event.latLng,
+                        map: map
+                    });
+
+                    building.on("deselect", function() {
+                        marker.setIcon(inactiveIcon);
+                    });
+
+                    var selectBuilding = function() {
+                        marker.setIcon(activeIcon);
+                        collection.trigger("select", building);
+                    };
+                    
+                    google.maps.event.addListener(marker, 'click', selectBuilding);
+
+                    building.on("destroy", function() {
+                        marker.setMap(null);
+                    });
+
+                    collection.add(building);
+
+                    selectBuilding();
+                }
                 
                 return false;
             });
