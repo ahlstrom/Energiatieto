adb57a78156e4dad643d643ddead1fdf10a1424f
Lauri Piispanen <lauri.piispanen@reaktor.fi>
2012-11-16 17:04:09 +0200

    show type of producer on form

--- src/app/js/views/form/productionform.js
+++ src/app/js/views/form/productionform.js
@@ -5,6 +5,21 @@ define([
     "text!../helptexts/production.txt"
     ], function(Marionette, tmpl, HelpTextVent, HelpText) {
         return Marionette.ItemView.extend({
+            templateHelpers: {
+                typeName: function() {
+                    if (this.type === "solarpanel") {
+                        return "Aurinkopaneeli";
+                    } else {
+                        return "Maalämpökaivo";
+                    }
+                }
+            },
+            events: {
+                "click .delete": "destroyModel"
+            },
+            destroyModel: function() {
+                this.model.destroy();
+            },
             template: {
                 template: tmpl,
                 type: "handlebars"
